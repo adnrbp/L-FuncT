be43a2061c1d873735e4a8efd73fd5c3f650002e
adnrbp <adnrbp@users.noreply.github.com>
2018-11-26 22:22:08 -0500

    [proj6] Restrictions for Lending Money

---
+++ 03Lend/Lending.hs
@@ -0,0 +1,23 @@
+-- whether or not lend some money to a customer
+
+-- using let to define
+lend amount balance = let reserve 	= 100
+						newBalance = balance - amount
+					  in if balance < reserve
+					  	then Nothing
+					  	else Just newBalance
+
+-- using where to define
+lend2 amount balance = if amount < reserve * 0.5
+						then Just newBalance
+						else Nothing
+	where reserve 	= 100
+			newBalance = balance - amount
+
+-- restrictions for lending
+lend3 amount balance
+	| amount <= 0 			 = Nothing
+	| amount > reserve * 0.5 = Nothing
+	| otherwise 			 = Just newBalance
+	where reserve	= 100
+		newBalance = balance - amount
