8d540cc85586f4cb7f8da9793d54438bf93ef023
adnrbp <adnrbp@users.noreply.github.com>
2018-11-28 22:23:48 -0500

    [proj8] Split Lines in text with Carriage return and newline '\r\n'

---
+++ 04SplitLines/SplitLines.hs
@@ -0,0 +1,13 @@
+
+-- from string return a list of separated strings
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminator cs
+    in pre : case suf of
+                ('\r':'\n':rest) -> splitLines rest
+                ('\r':rest) -> splitLines rest
+                ('\n':rest) -> splitLines rest
+                _  				 -> []
+
+isLineTerminator c = c == '\r' || c == '\n'
